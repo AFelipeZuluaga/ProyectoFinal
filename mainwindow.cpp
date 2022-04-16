4af917b07fa37f758d647d734624346fcbabe5cc
AFelipeZuluaga <felipe.zuluaga1@udea.edu.co>
2022-04-16 14:04:44 -0500

    Actualización

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -14,13 +14,15 @@ MainWindow::MainWindow(QWidget *parent)
     x = screenGeometry.x();
     y = screenGeometry.y();
 
-    scene=new QGraphicsScene(x,y,1081,761);
+    scene=new QGraphicsScene(x,y,1090,770);
 
     QGraphicsView* view = new QGraphicsView(scene);
     ui->graphicsView->setBackgroundBrush(Qt::darkGray);
 
     ui->graphicsView->setScene(scene);
 
+    QPushButton* N1= new QPushButton();
+//    scene->addItem(N1);
 
 }
 
@@ -33,10 +35,92 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_Nivel1_clicked()
 {
-    ui->graphicsView->setBackgroundBrush(Qt::yellow);
+    ui->graphicsView->setBackgroundBrush(Qt::darkGreen);
     ui->graphicsView->setScene(scene);
+
+    ui->stackedWidget->setCurrentIndex(1);
+
+
+
+    //Creación de objetos estáticos
+    barra1=new QGraphicsRectItem(35, 70, 20, 670);
+    scene->addItem(barra1);
+    barra1->setBrush(Qt::yellow);
+
+    barra2=new QGraphicsRectItem(1030, 70, 20, 670);
+    scene->addItem(barra2);
+    barra2->setBrush(Qt::yellow);
+
 }
 
+void MainWindow::on_Nivel2_clicked()
+{
+    ui->graphicsView->setBackgroundBrush(Qt::darkGreen);
+    ui->graphicsView->setScene(scene);
+
+    ui->stackedWidget->setCurrentIndex(2);
+
+
+    //Creación de objetos estáticos
+    barra1=new QGraphicsRectItem(35, 70, 20, 670);
+    scene->addItem(barra1);
+    barra1->setBrush(Qt::yellow);
+
+    barra2=new QGraphicsRectItem(1030, 70, 20, 670);
+    scene->addItem(barra2);
+    barra2->setBrush(Qt::yellow);
+
+}
+
+void MainWindow::on_Nivel3_clicked()
+{
+    ui->graphicsView->setBackgroundBrush(Qt::darkGreen);
+    ui->graphicsView->setScene(scene);
+
+    ui->stackedWidget->setCurrentIndex(3);
+
+
+    //Creación de objetos estáticos
+    barra1=new QGraphicsRectItem(35, 70, 20, 670);
+    scene->addItem(barra1);
+    barra1->setBrush(Qt::yellow);
+
+    barra2=new QGraphicsRectItem(1030, 70, 20, 670);
+    scene->addItem(barra2);
+    barra2->setBrush(Qt::yellow);
+
+}
+
+void MainWindow::on_Inicio1_clicked()
+{
+    ui->graphicsView->setBackgroundBrush(Qt::darkGray);
+    ui->graphicsView->setScene(scene);
+
+    ui->stackedWidget->setCurrentIndex(0);
+
+    barra1->hide();
+    barra2->hide();
+}
+
+void MainWindow::on_Inicio2_clicked()
+{
+    ui->graphicsView->setBackgroundBrush(Qt::darkGray);
+    ui->graphicsView->setScene(scene);
+
+    ui->stackedWidget->setCurrentIndex(0);
+    barra1->hide();
+    barra2->hide();
+}
+
+void MainWindow::on_Inicio3_clicked()
+{
+    ui->graphicsView->setBackgroundBrush(Qt::darkGray);
+    ui->graphicsView->setScene(scene);
+
+    ui->stackedWidget->setCurrentIndex(0);
+    barra1->hide();
+    barra2->hide();
+}
 
 void MainWindow::on_Salir_clicked()
 {
--- mainwindow.h
+++ mainwindow.h
@@ -28,12 +28,22 @@ private slots:
     void on_pushButton_clicked();
 
     void on_Nivel1_clicked();
+    void on_Nivel2_clicked();
+    void on_Nivel3_clicked();
+    void on_Inicio1_clicked();
+    void on_Inicio2_clicked();
+    void on_Inicio3_clicked();
 
     void on_Salir_clicked();
+
+
 
 private:
     Ui::MainWindow *ui;
     QGraphicsScene* scene;
+    QGraphicsRectItem* barra1;
+    QGraphicsRectItem* barra2;
+
 
         float x,y,ancho,alto;
 };
